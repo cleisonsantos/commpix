16121ade9654c03dcc092ad3d4f15ce0f9f3bdb0
cleisonsantos <css.cleison@outlook.com>
2020-12-13 09:46:34 -0300

    Estilização da navbar.

--- src/App.js
+++ src/App.js
@@ -5,20 +5,9 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Hello World</h1>
+      </div>
     </div>
   );
 }
