5897ebdf5d966483da8379b62e0215753d6a67be
cleisonsantos <css.cleison@outlook.com>
2020-12-15 22:34:48 -0300

    Algumas estilizações do formulário de serviços.

--- src/components/ServiceForm.jsx
+++ src/components/ServiceForm.jsx
@@ -2,10 +2,17 @@ import styled from 'styled-components'
 
 const Form = styled.form`
     
+    @media(max-width: 768px) {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
     
+
     display: flex;
-    flex-direction: column;
-    justify-content: center;
+    flex-direction: row;
+    justify-content: space-around;
+    margin-bottom: 1rem;
     
     input {
         font-size: 18px;
@@ -14,22 +21,37 @@ const Form = styled.form`
         border-radius: .25rem;
         border: 1px solid #ccc;
         margin: .25rem 0;
+        width: 100%;
+        margin: .25rem;
     }
     input:focus{
         border-color: #006c86;
         box-shadow: 0 0 0 0.2rem  #006b8660
     }
+    button {
+        font-size: 18px;
+        padding: .5rem;
+        color: #444;
+        border-radius: .25rem;
+        border: 1px solid #ccc;
+        margin: .25rem 0;
+        width: 100%;
+        margin: .25rem;
+    }
+    
 `
 
 function ServiceForm() {
     return (
-        <>
+        <div>
             <Form>
-                <input type="text" />
-                <input type="text" />
-                <input type="text" />
+                <input type="text" placeholder="Serviço"/>
+                <input type="text" placeholder="Descrição"/>
+                <input type="text" placeholder="Valor"/>
+                <button>Adicionar Serviço</button>
+                <button>Limpar</button>
             </Form>
-        </>
+        </div>
     )
 }
 
