be2c514d74a18e675bd7a3725c3df575dd5112b6
cleisonsantos <css.cleison@outlook.com>
2020-12-16 01:07:48 -0300

    Adicionado o componente de formulário

--- src/pages/Keys.jsx
+++ src/pages/Keys.jsx
@@ -1,10 +1,11 @@
-
+import KeyForm from '../components/KeyForm'
 
 
 function Keys() {
     return(
         <div>
             <h2>Minhas Chaves Pix</h2>
+            <KeyForm />
         </div>
     )
 }
