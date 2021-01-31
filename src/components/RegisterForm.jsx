00dbf2b25c15b737ac2720a65f51f62b42f403ca
cleisonsantos <css.cleison@outlook.com>
2021-01-31 16:23:56 -0300

    Registro de usuarios no sistema funcional.

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,20 +1,56 @@
+import React, { useState,  } from 'react';
+import api from "../services/api"
 import styled from 'styled-components'
 
-function RegisterForm() {
-    return(
-        <div>
-            <form>
-              <input type="text" placeholder="Nome" />
-              <input type="email" placeholder="E-mail" />
-              <input type="text" placeholder="Nome da Empresa" />
-              <input type="text" placeholder="Cargo" />
-              <input type="password" placeholder="Senha" />
-              <input type="password" placeholder="Confirmar Senha" />
-              <button type="submit" >Cadastrar</button>
-              <button type="reset" >Limpar</button>
-            </form>
-        </div>
-    )
-}
+const Form = styled.form`
 
+`
+
+function RegisterForm() {
+
+	const initialFormState = {
+		name: '',
+		email: '',
+		company: '',
+		office: '',
+		password: '',
+		conf_password: ''
+	}
+
+	// const [input, setInput] = useState('')
+
+	const [form, setForm] = useState(initialFormState)
+
+	const setInput = (newValue) => {
+		setForm(form => ({ ...form, ...newValue }))
+	}
+
+
+	const handleRegister = e => {
+		e.preventDefault();
+
+		api.post('/users', form).then(res => {
+			console.log(res)
+		}).catch(err => {
+			console.log(err)
+		})
+
+	}
+
+
+	return(
+		<div>
+		<Form onSubmit={handleRegister}>
+		<input name="name" type="text" placeholder="Nome" onChange={e => setInput({ name: e.target.value })}/>
+		<input name="email" type="email" placeholder="E-mail" onChange={e => setInput({ email: e.target.value })}/>
+		<input name="company" type="text" placeholder="Nome da Empresa" onChange={e => setInput({ company: e.target.value })}/>
+		<input name="office" type="text" placeholder="Cargo" onChange={e => setInput({ office: e.target.value })}/>
+		<input name="password" type="password" placeholder="Senha" onChange={e => setInput({ password: e.target.value })}/>
+		<input name="conf_password" type="password" placeholder="Confirmar Senha" onChange={e => setInput({ conf_password: e.target.value })}/>
+		<button type="submit" >Cadastrar</button>
+		<button type="reset" >Limpar</button>
+		</Form>
+		</div>
+	)
+}
 export default RegisterForm
